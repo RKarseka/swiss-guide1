f1382abfbac950e57f0abe42ea572375db7fc54c
raman <raman.karseka@gmail.com>
2025-04-17 00:47:53 +0300

    feat: added eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,91 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// @ts-check
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import globals from 'globals';
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import pluginReact from 'eslint-plugin-react';
+import pluginReactCompiler from 'eslint-plugin-react-compiler';
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+
+export default [
+  js.configs.recommended,
+  pluginPrettierRecommended,
+
+  ...ts.configs.recommended,
+  ...ts.configs.stylistic,
+
+  {
+    name: 'eslint-plugin-react/recommended',
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+    },
+  },
+
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      'jsx-a11y': pluginJsxA11y,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginJsxA11y.flatConfigs.recommended.rules,
+      'react-hooks/exhaustive-deps': 'off',
+    },
+  },
+
+  {
+    name: 'react-compiler/recommended',
+    plugins: {
+      'react-compiler': pluginReactCompiler,
+    },
+    rules: {
+      'react-compiler/react-compiler': 'error',
+    },
+  },
+
+  {
+    ignores: ['./dist', './node_modules'],
+    files: ['**/*'],
+    settings: {
+      react: { version: 'detect' },
+      componentWrapperFunctions: ['observer', 'styled'],
+    },
+    languageOptions: {
+      parserOptions: { ecmaFeatures: { jsx: true } },
+      globals: globals.browser,
+    },
+  },
+
+  {
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          singleQuote: true,
+          trailingComma: 'es5',
+          printWidth: 120,
+          endOfLine: 'auto',
+
+          jsxSingleQuote: true,
+          bracketSameLine: false,
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+    },
+  },
 ];
-
-export default eslintConfig;
