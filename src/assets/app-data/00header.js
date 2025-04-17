492f06427b569efed4e28c6f9c4815275e3d735f
raman <raman.karseka@gmail.com>
2025-04-17 23:19:56 +0300

    feat: added app-data header

---
+++ src/assets/app-data/00header.js
@@ -0,0 +1,9 @@
+export default {
+  pages: [
+    { key: 'why', priority: 0, link: 'why-me' },
+    { key: 'tours', priority: 1, link: 'tours' },
+    { key: 'reviews', priority: 2, link: 'reviews' },
+    { key: 'blog', priority: 3, link: 'blog' },
+    { key: 'contacts', priority: 4, link: 'contacts' },
+  ],
+};
